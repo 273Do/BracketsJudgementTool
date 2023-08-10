3ec9c6d0a460ff00625d990d7271c9e0b66fe655
273* <keita.y310@icloud.com>
2023-08-10 17:28:19 +0900

    コード追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const Brackets: { [key: string]: string } = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+
+  const [result, setResult] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const str: string = e.target.value;
+    const stack: string[] = [];
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] in Brackets) {
+        stack.push(str[i]);
+      } else {
+        if (
+          str.length % 2 !== 0 ||
+          str.length === 0 ||
+          stack.length === 0 ||
+          str[i] !== Brackets[stack.pop()!]
+        ) {
+          setResult(false);
+          return;
+        }
+      }
+    }
+    if (str.length === 0) setResult(false);
+    else setResult(stack.length === 0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BracketsJudgementTool</h1>
+      <div>
+        <p>小括弧'()' / 中括弧'｛｝' / 大括弧'[]'を入力してください。</p>
+        <form>
+          <input type="text" onChange={(e) => handleChange(e)} />
+        </form>
+        <p>判定：{String(result)}</p>
+      </div>
     </div>
   );
 }
